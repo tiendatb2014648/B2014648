4cc0101cf4b7a32a0f8cdca9e6cb3d191ba4c406
tiendatb2014648 <datb2014648@student.ctu.edu.vn>
2024-04-23 23:11:30 -0800

    Them va hieu chinh du an

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppHeader2 from "@/components/AppHeader2.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppHeader2,
+  },
+};
+</script>
+<template>
+  <div id="app">
+    <router-view name="header" />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
---
+++ src/components/AppHeader3.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Ứng dụng Quản lý mượn sách</a>
+        <div class="mr-auto navbar-nav">
+            <div class="nav-item">
+                <router-link :to="{ name: 'bookmanager' }" class="nav-link">
+                    Sách <i class="fas fa-book"></i>
+                </router-link>
+            </div>
+            <div class="nav-item">
+                <router-link :to="{ name: 'nxbmanager' }" class="nav-link">
+                    Nhà xuất bản <i class="fas fa-building"></i>
+                </router-link>
+            </div>
+        </div>
+    </nav>
+</template>
+
